c2695b451e964db497f465a88afe89f090d267b7
Aja Washington <57641436+ajawashington@users.noreply.github.com>
2021-01-30 09:19:33 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-// STEPS NEEDED TO COMPLETE GAME
-//ask player names and assign colors
 
-//function to change (not toggle) color of button on click
-
-//function to check the bottom, most available row
-
-//function to check inputs of the same color for wins
-//horizontal, vertical, and diagonal wins
-
-//function to check when game is finish
-//which someone has won or all inputs are full
-
-//game logic, start with player 1, toggle to player 2, consistenly
-// checking for wins, once game is won or over, ask to play again?
-// -------------------------------------------------------------------------------
-
-//START GAME, CREATING A PLAYER
   var player1 = prompt("Player One: Enter your Name. Your color is Orange")
   var player1Color = 'rgb(255, 165, 0)';
 
@@ -25,23 +8,19 @@
   var game_on = true;
 var table = $('table tr');
 
-// http://stackoverflow.com/questions/6139407/getting-td-by-index-with-jquery
 function reportWin(rowNum,colNum) {
   console.log("You won starting at this row,col");
   console.log(rowNum);
   console.log(colNum);
 }
-// Change the color of a button
 function changeColor(rowIndex,colIndex,color) {
   return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color',color);
 }
 
-// Report Back to current color of a button
 function returnColor(rowIndex,colIndex) {
   return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
 }
 
-// Take in column index, returns the bottom row that is still gray
 function checkBottom(colIndex) {
   var colorReport = returnColor(5,colIndex);
   for (var row = 5; row > -1; row--) {
@@ -52,12 +31,11 @@ function checkBottom(colIndex) {
   }
 }
 
-// Check to see if 4 inputs are the same color
+
 function colorMatchCheck(one,two,three,four){
   return (one===two && one===three && one===four && one !== 'rgb(128, 128, 128)' && one !== undefined);
 }
 
-// Check for Horizontal Wins
 function horizontalWinCheck() {
   for (var row = 0; row < 6; row++) {
     for (var col = 0; col < 4; col++) {
@@ -72,7 +50,7 @@ function horizontalWinCheck() {
   }
 }
 
-// Check for Vertical Wins
+
 function verticalWinCheck() {
   for (var col = 0; col < 7; col++) {
     for (var row = 0; row < 3; row++) {
@@ -87,7 +65,6 @@ function verticalWinCheck() {
   }
 }
 
-// Check for Diagonal Wins
 function diagonalWinCheck() {
   for (var col = 0; col < 5; col++) {
     for (var row = 0; row < 7; row++) {
@@ -106,7 +83,7 @@ function diagonalWinCheck() {
   }
 }
 
-// Game End
+
 function gameEnd(winningPlayer) {
   for (var col = 0; col < 7; col++) {
     for (var row = 0; row < 7; row++) {
@@ -117,34 +94,34 @@ function gameEnd(winningPlayer) {
   }
 }
 
-// Start with Player One
+
 var currentPlayer = 1;
 var currentName = player1;
 var currentColor = player1Color;
 
-// Start with Player One
+
 $('h3').text(player1+": it is your turn, please pick a column to drop your blue chip.");
 
 $('.board button').on('click',function() {
 
-  // Recognize what column was chosen
+  
   var col = $(this).closest("td").index();
 
-  // Get back bottom available row to change
+  
   var bottomAvail = checkBottom(col);
 
-  // Drop the chip in that column at the bottomAvail Row
+
   changeColor(bottomAvail,col,currentColor);
 
-  // Check for a win or a tie.
+
   if (horizontalWinCheck() || verticalWinCheck() || diagonalWinCheck()) {
     gameEnd(currentName);
   }
 
-  // If no win or tie, continue to next player
+
   currentPlayer = currentPlayer * -1 ;
 
-  // Re-Check who the current Player is.
+
   if (currentPlayer === 1) {
     currentName = player1;
     $('h3').text(currentName+": it is your turn, please pick a column to drop your blue chip.");
